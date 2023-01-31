adec9a98f347866f9de2e9e6d85dadb884c271dd
Valentin <vflorea97@yahoo.com>
2023-01-31 20:49:04 +0200

    create pantofisport

---
+++ src/main/java/ro/mycode/PantofiSport/PantofiSportApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.PantofiSport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantofiSportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantofiSportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/PantofiSport/PantofiSportApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.PantofiSport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantofiSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
